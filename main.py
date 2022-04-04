96adf1d70e401c4823887a93ee373bc5fd315cca
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-04-05 00:11:54 +0100

    Playing around with absolute and relative paths

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+DEFAULT_NAME = "[name]"
 
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# with open("./Input/Names/invited_names.txt", mode="r") as name_invites:
+#     print(f.read)
+
+names_list = open("./Input/Names/invited_names.txt", mode="r")
+
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_content = letter_file.read()
+    for name in names_list:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(DEFAULT_NAME, stripped_name)
+        print(new_letter)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as final_letter:
+            final_letter.write(new_letter)
+
